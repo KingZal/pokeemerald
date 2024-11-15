8d209bb9c824bcb16389fbb2157bae7b2949be56
KingZal <armeen.noorshahi@gmail.com>
2024-11-14 23:39:44 -0500

    a grilled cheese sandwich
    Added a scripted introduction to the apartment greeter

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -654,7 +654,7 @@
 
 #define FLAG_BUDDY_0x264  0x264 // Used Flag
 #define FLAG_TEST_CALL_0x265  0x265 // Used Flag
-#define FLAG_UNUSED_0x266  0x266 // Unused Flag
+#define FLAG_LAVENDER_0x266  0x266 // Used Flag
 #define FLAG_UNUSED_0x267  0x267 // Unused Flag
 #define FLAG_UNUSED_0x268  0x268 // Unused Flag
 #define FLAG_UNUSED_0x269  0x269 // Unused Flag
